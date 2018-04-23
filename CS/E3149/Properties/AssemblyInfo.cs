7799ce089c25ae2e65d187c80ee203c255ea30cb
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/E3149/Properties/AssemblyInfo.cs
@@ -0,0 +1,51 @@
+﻿// Developer Express Code Central Example:
+// How to provide sorting functionality for the grid that allows drag and drop row reordering
+// 
+// This example extends the example provided in the
+// http://www.devexpress.com/scid=E764 article with sorting functionality.
+// 
+// You can find sample updates and versions for different programming languages here:
+// http://www.devexpress.com/example=E3149
+
+// Developer Express Code Central Example:
+// How to reorder grid rows by drag and drop
+// 
+// This example extends the example provided in the
+// http://www.devexpress.com/scid=A2343 article with drag and drop functionality.
+// 
+// You can find sample updates and versions for different programming languages here:
+// http://www.devexpress.com/example=E764
+
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("E764")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("E764")]
+[assembly: AssemblyCopyright("Copyright ©  2010")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("ff0c7519-bd27-4bfc-8b8d-99f8eaf7ef4f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
